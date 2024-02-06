3972626dac1612dd188e22bb664b82573fc580d1
connorsabine <connorsabine@icloud.com>
2024-02-06 16:05:27 -0500

    [UPDATE] added code for vision and apriltags

--- src/main/java/frc/robot/commands/VisionCommand.java
+++ src/main/java/frc/robot/commands/VisionCommand.java
@@ -1,5 +1,23 @@
 package frc.robot.commands;
 
-public class VisionCommand {
-    
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+
+public class VisionCommand extends Command {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final VisionSubsystem m_visionSubsystem;
+
+    public VisionCommand(VisionSubsystem subsystem){
+        m_visionSubsystem = subsystem;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize(){
+    }
+
+    @Override
+    public void execute(){
+    }
 }
--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,5 +1,36 @@
 package frc.robot.subsystems;
 
-public class VisionSubsystem {
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants;
+
+
+public class VisionSubsystem extends SubsystemBase{
+    public NetworkTable table;
+    public NetworkTableEntry tx;
+    public NetworkTableEntry ty;
+    public NetworkTableEntry ta;
+
+    public VisionSubsystem(){
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    @Override
+    public void periodic(){
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double a = ta.getDouble(0.0);
+
+        SmartDashboard.putNumber("April Tag X", x);
+        SmartDashboard.putNumber("April Tag Y", y);
+        SmartDashboard.putNumber("April Tag Area", a);
+    }
+
     
 }
